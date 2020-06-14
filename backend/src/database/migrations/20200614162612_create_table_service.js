2adb5075c8bc868669d76f99f66400c5400b62d8
victor-medeiros <victor.msilva@bandtec.com.br>
2020-06-14 20:09:24 -0300

    Adição da migration responsável por criar a tabela service

---
+++ backend/src/database/migrations/20200614162612_create_table_service.js
@@ -0,0 +1,15 @@
+exports.up = knex => knex.schema.createTable('service', table => {
+      table.increments('id');
+      table.integer('client_id').notNullable().unsigned();
+      table.integer('professional_id').unsigned();
+      table.datetime('dateTime');
+      table.decimal('price');
+      table.string('request');
+      table.string('solution');
+      table.string('status');
+
+      table.foreign('client_id').references('user.id');
+      table.foreign('professional_id').references('user.id');
+  });
+
+exports.down = knex => knex.schema.dropTable('service');
